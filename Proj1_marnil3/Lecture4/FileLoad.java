554340f9965060af4c305c69e0b1c99affd9025b
Martin Nilsson <marnil3@UG.KTH.SE>
2017-03-28 17:46:22 +0200

    hello

---
+++ Proj1_marnil3/Lecture4/FileLoad.java
@@ -0,0 +1,43 @@
+package Lecture4;
+import java.util.*;
+import java.io.*;
+
+public class FileLoad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String dataFile = "iris.csv";
+		// Define buffer and split element
+		
+		//print
+		System.out.println("" + dataFile);
+	
+		
+		
+		//FileReader fr = new FileReader(dataFile);
+		BufferedReader br = new BufferedReader(fr); 
+		
+		
+		// Create and ArrayList for Flowers (flowerList)
+		//try {
+		//br = ; // load a file into buffer
+		// Cycle through the file
+		//while ((line = br.readLine()) != null) {
+		// Read each line of file and store data into database
+		//}
+		//} catch (FileNotFoundException e) {
+		//e.printStackTrace();
+		//} catch (IOException e) {
+		//e.printStackTrace();
+		//}
+		// Print out fields of flowerList elements. To get element of ArrayList, command
+		//flowerList.get(i) can be used. Remember to read fields of the objects.
+		 //}
+		//}
+
+	//}
+	//public class Flower {
+		// Define a class Flower with a constructor.
+		}
+
+}
