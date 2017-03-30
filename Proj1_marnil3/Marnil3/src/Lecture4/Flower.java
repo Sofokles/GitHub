843ad0a512c416153f4c1771dc44b64b0397bcea
Martin Nilsson <marnil3@UG.KTH.SE>
2017-03-30 14:38:14 +0200

    again

---
+++ Proj1_marnil3/Marnil3/src/Lecture4/Flower.java
@@ -0,0 +1,16 @@
+package Lecture4;
+
+public class Flower{
+	// Define a class Flower with a constructor.		
+			String type;
+			String sepal_length;
+			String sepal_width;
+			String petal_length;
+			String pepal_width;
+			//Create a constructor
+			Flower(){			
+			}
+			//Flower(String type, String sepal_length, String sepal_width, String petal_length, String pepal_width){
+					
+			//}
+}
