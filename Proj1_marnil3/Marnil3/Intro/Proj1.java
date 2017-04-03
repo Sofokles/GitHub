c8a7e292257b357326e300f5e166a29f5aefeac3
Martin Nilsson <marnil3@UG.KTH.SE>
2017-04-03 14:42:27 +0200

    again

--- Proj1_marnil3/Marnil3/Intro/Proj1.java
+++
@@ -1,8 +0,0 @@
-package Intro;
-
-public class Proj1 {
-
-	public static void main(String[] args) {
-		System.out.println("Hello EH2745 class"); }
-
-}
--- Proj1_marnil3/Marnil3/LectureII/Manager.java
+++
@@ -1,8 +0,0 @@
-package LectureII;
-
-
-public class Manager extends Employee {
-	// Create a constructor for the class with an additional field officeNumber
-	String officenumber;
-	
-	}
